66b4a9a69d39b14039233f2b6f33a2a73c1b75a9
yikai.jin <yikai.jin@anmav.com>
2019-11-01 20:26:37 +0800

    Title update

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = `BlockAPI-Docs-EN <https://iblockapi.com>`_
+project = 'BlockAPI-Docs-EN'
 copyright = '2019'
 author = 'iBlockAPI'
 
